e9005e8eb9870c8736f6b268d712d66b854ad26e
panglokchi <lokchi.pang@gmail.com>
2025-04-05 03:05:05 +0100

    added pagination for bird list

--- src/app/bird-list/bird-list.component.ts
+++ src/app/bird-list/bird-list.component.ts
@@ -1,12 +1,13 @@
-import { Component, inject, input, Input } from '@angular/core';
+import { Component, inject, input, Input, HostListener } from '@angular/core';
 import { GameService } from '../game.service';
 import { CommonModule } from '@angular/common';
 import { BirdCardComponent } from '../bird-card/bird-card.component';
 import { Bird } from '../bird';
+import { NgbPagination } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-bird-list',
-  imports: [CommonModule, BirdCardComponent],
+  imports: [CommonModule, BirdCardComponent, NgbPagination],
   templateUrl: './bird-list.component.html',
   styleUrl: './bird-list.component.css'
 })
@@ -15,7 +16,41 @@ export class BirdListComponent {
   gameService = inject(GameService)
   public birdList: Bird[] = [];
   public birdListActive: Bird[] = [];
+  public paginationElements: number = 12;
+  public currentPage: number = 1;
+
+  Math = Math
+  Array = Array
+
+  updatePagination() {
+    console.log(window.innerWidth, window.innerHeight)
+    let px = 0;
+    let py = 0;
+    if (window.innerWidth >= 992) {
+      px = 6;
+    } else {
+      px = 3;
+    }
+    if (window.innerHeight >= 1400) {
+      py = 5;
+    } else if (window.innerHeight >= 992) {
+      py = 3;
+    } else {
+      py = 2;
+    }
+    this.paginationElements = px * py;
+  }
+
+  //@HostListener('window:resize', ['$event'])
+  //onResize(event: any) {
+  //  console.log("window resize")
+  //  this.updatePagination();
+  //}
+  // not working
+  
   constructor() {
+    this.updatePagination();
+
     this.gameService.getBirdList().subscribe({
       next: res => {
         this.birdList = res.filter((b: any) => b.assigned_to_coop == false);
